2ee5c55aecc829d3b3f6d306e74433140085a8e8
Enrique <enrique.barbosasilva@gmail.com>
2022-09-30 18:31:43 -0300

    feat: new-selection-created

--- selects.sql
+++ selects.sql
@@ -23,8 +23,14 @@ SELECT p.id id_pr_frita FROM produtos_pedidos pp
 WHERE 
     pr.nome = 'Fritas'
 
-
 -- 3)
+
+SELECT client.nome gostam_de_fritas FROM produtos_pedidos pp
+	INNER JOIN produtos pr ON pp.produto_id = pr.id
+    INNER JOIN pedidos p ON pp.pedido_id = p.id
+    INNER JOIN clientes client ON p.cliente_id = client.id
+WHERE 
+	pr.nome = 'Fritas'
 
 -- 4)
 
