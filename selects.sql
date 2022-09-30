b1edf4456b30968af5c1be975c8fd122815bbe2d
Enrique <enrique.barbosasilva@gmail.com>
2022-09-30 18:23:09 -0300

    feat: two-new-results-for-selections

--- selects.sql
+++ selects.sql
@@ -3,7 +3,26 @@
 
 -- 1)
 
+SELECT 
+	pr.id,
+    pr.status, 
+    pr.cliente_id, 
+    prod.id, prod.nome, 
+    prod.tipo, prod.preco, 
+    prod.pts_de_lealdade 
+    
+FROM produtos_pedidos pp 
+    INNER JOIN produtos prod ON pp.produto_id = prod.id 
+    INNER JOIN pedidos pr ON pp.pedido_id = pr.id;
+
 -- 2)
+
+SELECT p.id id_pr_frita FROM produtos_pedidos pp
+	INNER JOIN produtos pr ON pp.produto_id = pr.id
+    INNER JOIN pedidos p ON pp.pedido_id = p.id
+WHERE 
+    pr.nome = 'Fritas'
+
 
 -- 3)
 
