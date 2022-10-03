f358003c583dd64684859e2d16e75bfbcac3058f
Enrique <enrique.barbosasilva@gmail.com>
2022-10-03 12:21:04 -0300

    feat: update-on-crud-created

--- crud.sql
+++ crud.sql
@@ -42,8 +42,21 @@ WHERE pp.pedido_id = 6;
 
 -- 1)
 
-
-
+UPDATE clientes
+  SET lealdade = (
+    SELECT
+    sum(prod.pts_de_lealdade)
+    FROM
+      pedidos p
+      INNER JOIN produtos_pedidos pp ON p.id = pp.pedido_id
+      INNER JOIN produtos prod ON prod.id = pp.produto_id
+      INNER JOIN clientes c ON c.id = p.cliente_id
+      WHERE c.nome LIKE 'Georgia'
+    GROUP BY
+      c.nome)
+WHERE
+  id = 6
+  
 -- Deleção
 
 -- 1)
